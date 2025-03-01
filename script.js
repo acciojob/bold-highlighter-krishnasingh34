7ad71098702dff2421d8e406c058e575f1b80e4b
Krishna Singh <144099384+krishnasingh34@users.noreply.github.com>
2025-03-01 16:42:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+let strongTags = document.querySelectorAll('strong')
+
 function highlight() {
     //Write your code here
-
-
+	for(let t of strongTags){
+		t.style.color = 'rgb(0, 128, 0)'
+	}
 }
-
 
 function return_normal() {
     //Write your code here
-
-    
+		for(let t of strongTags){
+		t.style.color = 'rgb(0, 0, 0)'
+	} 
 }
+
+
